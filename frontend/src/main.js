cd6d92ac3d84fa0740e898833d37f0d402adc70e
GarrettCrowner <123480712+GarrettCrowner@users.noreply.github.com>
2026-04-20 20:54:31 -0400

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,18 @@
 import { router } from "./utils/router.js";
 import { renderNavbar } from "./components/navbar.js";
 import { renderTabBar } from "./components/tabBar.js";
+import { setupPWA } from "./utils/pwa.js";
 
 renderNavbar();
 renderTabBar(router.navigate);
 router.init();
+
+// Set up push notifications if user is logged in
+if (localStorage.getItem("token")) {
+  setupPWA().catch(() => {});
+}
+
+// Also set up after login
+window.addEventListener("user-logged-in", () => {
+  setupPWA().catch(() => {});
+});
